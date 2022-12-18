5140b97112f5037f978f52bd47b67653b011903a
nilay <nilaynathsharan16@gmail.com>
2022-12-19 04:54:05 +0530

    feat: adding basic functionality

--- heka/src/main.rs
+++ heka/src/main.rs
@@ -1,4 +1,18 @@
 use std::io;
+use std::process::Command; // for executing command
+fn add_component(component_name: &str, project_name: &str) {
+    let path: String = String::from("/home/nilay/Projects/WebsiteComponents/") + component_name;
+    let projectpath: String = String::from("./") + project_name + "/src/" + component_name;
+    println!("project path - {}", projectpath);
+    println!("component path - {} ", path);
+    // cp <source> <destination>
+    Command::new("cp")
+        .arg("-r")
+        .arg(path)
+        .arg(projectpath)
+        .output()
+        .expect("Can't copy the file");
+}
 
 fn main() {
     println!("Enter the components :");
@@ -11,8 +25,8 @@ fn main() {
     for i in &components {
         let component: &str = i;
         match component.trim() {
-            "navbar" => println!("navbar"),
-            "footer" => println!("footer"),
+            "navbar" => add_component("NavBar", components[0]),
+            "footer" => add_component("footer", components[0]),
             _ => println!("fuck wrong input"),
         };
     }
