4aaed0032de02168f31836b6e888746162494d8e
nilay <nilaynathsharan16@gmail.com>
2022-12-19 02:40:56 +0530

    feat: adding input

--- heka/src/main.rs
+++ heka/src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter the components :");
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("failed to readline");
+    let components: Vec<&str> = input.split(' ').collect();
+
+    for i in &components {
+        let component: &str = i;
+        match component.trim() {
+            "navbar" => println!("navbar"),
+            "footer" => println!("footer"),
+            _ => println!("fuck wrong input"),
+        };
+    }
 }
+// /home/nilay/Projects/WebsiteComponents/NavBar
